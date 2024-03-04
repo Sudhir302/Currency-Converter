afcc4dca0dfec54104e529d7eff53556a8529cc7
Sudhir Chaudhary <149799377+Sudhir302@users.noreply.github.com>
2024-03-04 22:42:39 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,58 @@
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const fromCurr = document.querySelector("#fromCntry");
+const toCurr = document.querySelector(".to select");
+
+const valueMsg = document.querySelector(".value");
+
+
+let select = document.querySelectorAll("select");
+const btn = document.querySelector("button");
+
+for(let s of select){
+    for(currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if(s.name === "from" && currCode === "NPR"){
+            newOption.selected = "selected";
+        }
+        else if (s.name === "to" && currCode === "INR"){
+            newOption.selected = "selected";
+        }
+        s.append(newOption);
+    }
+    s.addEventListener('change', (evt) => {
+        flagUpdate(evt.target);
+    })
+}
+
+
+const flagUpdate = (element) =>{
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+
+
+btn.addEventListener("click", async(evt) =>{
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if(amtVal === "" || amtVal < 1){
+        amtVal= 1;
+        amount.value= "1";
+    }
+
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()]
+    
+
+    let finalAmount = amtVal * rate;
+    valueMsg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+.0
+});
